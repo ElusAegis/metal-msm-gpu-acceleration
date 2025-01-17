f77ce4710e7bf5485e2ee696466dad536686d37e
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-01-17 18:24:19 +0000

    fix: fix the ios benchmarking app

---
+++ ios-metal-benchmarker/ios-metal-benchmarker/ios_metal_benchmarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_metal_benchmarkerApp.swift
+//  ios-metal-benchmarker
+//
+//  Created by Artem Grigor on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_metal_benchmarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
